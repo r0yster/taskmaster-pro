fc7b76ea0c1d9e09e8b270278fe2dccb69d140e2
royster <rcantu786@gmail.com>
2021-04-26 01:29:25 -0500

    final touches

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,14 +55,14 @@ var auditTask = function(taskEl) {
   .trim();
 
   // convert to moment object at 5:00pm
-  var tiem = moment(date, "L").set("hour", 17);
+  var time = moment(date, "L").set("hour", 17);
 
   // remove any old classes from element
   $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
 
   // apply new class if task is near/over due date
   if (moment().isAfter(time)) {
-    $(taskEl).addClass.("list-group-item-danger");
+    $(taskEl).addClass("list-group-item-danger");
   } else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
   }
